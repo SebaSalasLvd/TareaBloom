e1dd8338cdf6d8c6eea54526f99c390e624b2078
Sebastián Salas Lavado <sebastian.salas.l@ug.uchile.cl>
2024-11-29 21:57:34 -0300

    Doc(react): Documented Home.

--- react/src/pages/Home.jsx
+++ react/src/pages/Home.jsx
@@ -2,21 +2,56 @@ import {useEffect, useState} from 'react';
 import axios from 'axios';
 import { PokemonCard } from '../components/PokemonCard';
 import { SearchBar } from '../components/SearchBar';
+/**
+ * Home component for searching and displaying details of a Pokémon.
+ * 
+ * This component allows the user to search for a Pokémon by name and displays the selected Pokémon's 
+ * name, types, and an image. It fetches the Pokémon data from the PokeAPI when a Pokémon is selected.
+ * 
+ * @component
+ */
 export const Home = () => {
+  /**
+   * State for storing the selected Pokémon's name.
+   * @type {string | null}
+   */
   const [selectedPokemon, setSelectedPokemon] = useState(null);
+  /**
+   * State for storing the selected Pokémon's information.
+   * @type {Object | null}
+   * @property {string} name - The name of the Pokémon.
+   * @property {Array<{type: string}>} types - The types of the Pokémon.
+   * @property {string} image - The URL of the Pokémon's image.
+   */
   const [pokemonInfo, setPokemonInfo] = useState(null);
 
+  /**
+   * Handles the selection of a Pokémon from the search bar.
+   * 
+   * @param {string} pokemonName - The name of the selected Pokémon.
+   */
   const handlePokemonSelect = (pokemonName) => {
     setSelectedPokemon(pokemonName);
     console.log(`Selected Pokémon: ${pokemonName}`);
   };
 
+  /**
+   * Effect hook to fetch Pokémon data when the selected Pokémon changes.
+   * Runs every time `selectedPokemon` is updated.
+   */
   useEffect(() => {
     if (selectedPokemon) {
       fetchPokemonInfo();
     }
   }, [selectedPokemon]);
 
+  /**
+   * Fetches the data for the selected Pokémon from the PokeAPI.
+   * 
+   * This function makes an API call to the PokeAPI to retrieve the selected Pokémon's data, including 
+   * its name, types, and image, and stores this information in the `pokemonInfo` state.
+   * If the fetch fails, it logs an error and alerts the user.
+   */
   const fetchPokemonInfo = async () => {
     try {
       const response = await axios.get(
