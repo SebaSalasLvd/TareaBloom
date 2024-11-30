6cf9b80b6420a209063ee246677b3aed909bb05e
Sebastián Salas Lavado <sebastian.salas.l@ug.uchile.cl>
2024-11-29 22:20:38 -0300

    Doc(react): Documented PokemonCard Component.

--- react/src/components/PokemonCard.jsx
+++ react/src/components/PokemonCard.jsx
@@ -1,5 +1,28 @@
 import { CustomChip } from "../templates/CustomChip";
+/**
+ * `PokemonCard` is a functional React component that displays a Pokémon's information, 
+ * including its name, types, and an image.
+ *
+ * @component
+ * @example
+ * // Example usage:
+ * <PokemonCard name="Pikachu" types={[{ type: "electric" }]} image="path_to_image" />
+ *
+ * @param {Object} props - The properties passed to the component.
+ * @param {string} props.name - The name of the Pokémon (e.g., "Pikachu").
+ * @param {Array} props.types - An array of objects representing the Pokémon's types. 
+ * Each object should have a `type` property indicating the type name (e.g., "electric").
+ * @param {string} props.image - The URL of the Pokémon's image.
+ *
+ * @returns {JSX.Element} - A JSX element representing the card with the Pokémon's information.
+ */
 export const PokemonCard = ({name, types, image}) => {
+    /**
+     * Capitalizes the first letter of a string.
+     * 
+     * @param {string} str - The string to capitalize.
+     * @returns {string} - The string with the first letter capitalized.
+     */
     const capitalizeFirstLetter = (str) => str.charAt(0).toUpperCase() + str.slice(1);
     return (
         <>
@@ -23,4 +46,4 @@ export const PokemonCard = ({name, types, image}) => {
             </div>
         </>
     );
-}
+};
